50774a9a68ec20dfa577a48bdb7167f559e6d896
RandyLizzy2012 <164670522+RandyLizzy2012@users.noreply.github.com>
2024-03-24 11:12:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
         { name: 'card4', img: 'images/rollsafe.png' },
         { name: 'card5', img: 'images/success.png' },
         { name: 'card5', img: 'images/success.png' },
+        { name: 'card6', img: 'images/IMG_1888' },
+        { name: 'card6', img: 'images/IMG_1888' },
         // ...add more pairs as needed
     ];
 
